afb44c3b3edc356bedad4cfab2579fbec8a0e5f0
Michael Lieberman <mlieberman85@gmaill.com>
2012-11-06 11:25:54 -0500

    Fixing server proxy route

--- routes/server.js
+++ routes/server.js
@@ -5,10 +5,13 @@
 exports.server = function (req, res) {
     var httpProxy = require('http-proxy');
     var proxy = new httpProxy.RoutingProxy();
+    url = req.params.server + '/server' + req.params.server;
+    proxyServerandPort = req.params.server + ':9091';
+    var options = {
+        router: {
+            url: proxyServerandPort
+        }
+    };  
     console.log("proxying to: " + req.params.server);
-    req.url = "/";
-    proxy.proxyRequest(req, res, {
-        host: req.params.server,
-        port: 9091
-    });
+    proxy.proxyRequest(req, res, options);
 };
