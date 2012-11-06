44a7c16cd7c4b35668c163f7bba92299832b5616
Chef User <chef-user@ip-10-77-103-12.ec2.internal>
2012-11-06 15:58:18 +0000

    Modified server route

--- routes/server.js
+++ routes/server.js
@@ -5,6 +5,8 @@
 exports.server = function (req, res) {
     var httpProxy = require('http-proxy');
     var proxy = new httpProxy.RoutingProxy();
+    console.log("proxying to: " + req.params.server);
+    req.url = "/";
     proxy.proxyRequest(req, res, {
         host: req.params.server,
         port: 9091
