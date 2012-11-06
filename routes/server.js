4578a5bdeebad628bc0a1bc4cee72ef8e3bd86a0
Michael Lieberman <mlieberman85@gmaill.com>
2012-11-06 11:34:41 -0500

    Still trying to find the right option to proxy url through a routing proxy.

--- routes/server.js
+++ routes/server.js
@@ -5,13 +5,10 @@
 exports.server = function (req, res) {
     var httpProxy = require('http-proxy');
     var proxy = new httpProxy.RoutingProxy();
-    url = req.params.server + '/server' + req.params.server;
+    url = '/server' + req.params.server;
     proxyServerandPort = req.params.server + ':9091';
-    var options = {
-        router: {
-            url: proxyServerandPort
-        }
-    };
     console.log("proxying to: " + req.params.server);
-    proxy.proxyRequest(req, res, options);
+    proxy.proxyRequest(req, res, require('proxy-by-url')({
+        url: {port: 9091, host: req.params.server}
+    }));
 };
