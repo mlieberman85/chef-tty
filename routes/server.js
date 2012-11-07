fc73b8046e14badc8a960c5cf6762eaf649fa2a4
Michael Lieberman <mlieberman85@gmaill.com>
2012-11-07 14:21:46 -0500

    Still trying to get this to route right.

--- routes/server.js
+++ routes/server.js
@@ -5,10 +5,9 @@
 exports.server = function (req, res) {
     var httpProxy = require('http-proxy');
     var proxy = new httpProxy.RoutingProxy();
-    url = '/server' + req.params.server;
-    proxyServerandPort = req.params.server + ':9091';
-    console.log("proxying to: " + req.params.server);
-    proxy.proxyRequest(req, res, require('proxy-by-url')({
-        url: {port: 9091, host: req.params.server}
-    }));
+    req.url = ('/');
+    proxy.proxyRequest(req, res, {
+        host: req.params.server,
+        port: 9091
+    });
 };
