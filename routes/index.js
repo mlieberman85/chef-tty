629cd50cf9f583477fc8abd06948e63f7d5d12f0
Michael Lieberman <mlieberman85@gmaill.com>
2012-11-08 14:46:27 -0500

    Still trying to get this to route right.

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@
 exports.index = function(req, res){
     var exec = require('child_process').exec,
         child, servers;
-    child = exec ('for i in `knife node list`; do knife node show $i; done | grep IP | sed s/IP://g | sed -e "s/^[ \\t]*//g"',
+    child = exec ('for i in `knife node list`; do knife node show $i; done | grep FQDN | sed s/FQDN://g | sed -e "s/^[ \\t]*//g"',
         function(error, stdout, stderr) {
             console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
